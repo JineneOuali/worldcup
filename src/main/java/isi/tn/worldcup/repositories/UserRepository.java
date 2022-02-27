6f6672c1d4da29ec2c0a0fdbc7b3bcf7cd908731
FatmaKahloun <100536092+FatmaKahloun@users.noreply.github.com>
2022-02-27 21:24:56 +0100

    Update UserRepository.java

--- src/main/java/isi/tn/worldcup/repositories/UserRepository.java
+++ src/main/java/isi/tn/worldcup/repositories/UserRepository.java
@@ -1,11 +1,11 @@
-package isi.tn.worldcup.repositories;
+package isi.tn.worldcup.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import isi.tn.worldcup.entities.User;
-
 @Repository
-public interface UserRepository extends JpaRepository <User, Long>{
+public interface userRepository extends JpaRepository<User,Long>{
+
 
 }
