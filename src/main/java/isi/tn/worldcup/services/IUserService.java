3dee8fbcf50faf2293eddc82ea5d57ae34a4a2b5
FatmaKahloun <100536092+FatmaKahloun@users.noreply.github.com>
2022-02-27 21:27:00 +0100

    Update IUserService.java

--- src/main/java/isi/tn/worldcup/services/IUserService.java
+++ src/main/java/isi/tn/worldcup/services/IUserService.java
@@ -1,12 +1,10 @@
 package isi.tn.worldcup.services;
 
 import java.util.List;
-
 import isi.tn.worldcup.entities.User;
 
 public interface IUserService {
-  
-	User saveUser(User user);
 
+	User saveUser(User user);
 	List<User> findAllUsers();
 }
